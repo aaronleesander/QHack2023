ab6c3e8b9c39df9748b68a8daa532411e27b9ce2
EmilianoG-byte <57567043+EmilianoG-byte@users.noreply.github.com>
2023-02-26 23:12:22 +0100

    add depolarizing noise model

--- src/noise_models.py
+++ src/noise_models.py
@@ -11,3 +11,16 @@ def apply_Pauli_noise(wire, noise_probability, noise_strength):
 
     if np.random.rand() < noise_probability: 
         qml.RZ(noise_strength, wires=wire)
+
+
+def apply_depolarizing(wire, p):
+    r = np.random.rand()
+    # divide range [0,1] in [0, p/3]: X; [p/3, 2p/3]: Y; [2p/3, p]: Z; [p, 1]: Id
+    if 0 <= r <= p/3:
+        qml.PauliX(wires=wire)
+    elif  p/3 < r <= 2*p/3:
+        qml.PauliY(wires=wire)
+    elif  2*p/3 < r <= p:
+        qml.PauliZ(wires=wire)
+    else: 
+        pass
