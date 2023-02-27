7b658f8777c65aaf22b2e00231d21e5aac1398f9
Aaron Sander <61705296+aaronleesander@users.noreply.github.com>
2023-02-27 18:36:06 +0100

    runtime now times over samples

--- src/tests.py
+++ src/tests.py
@@ -5,18 +5,19 @@ from timeit import default_timer as timer
 from .models import simulate_heisenberg_model, simulate_heisenberg_model_single_timestep
 
 
-def calculate_heisenberg_runtime_vs_qubits(backend, num_wires_list, couplings, T, depth, p):
+def calculate_heisenberg_runtime_vs_qubits(backend, num_wires_list, couplings, T, depth, p, samples):
     runtimes = []
     for wires in num_wires_list:
-        dev = qml.device(backend, wires=wires)
-
-        @qml.qnode(dev)
-        def heisenberg_trotter(couplings, T, depth, p):
-            simulate_heisenberg_model(wires, couplings, T, depth, p)
-            return qml.state()
-
         start = timer()
-        state = heisenberg_trotter(couplings, T, depth, p)
+        for _ in range(samples):
+            dev = qml.device(backend, wires=wires)
+
+            @qml.qnode(dev)
+            def heisenberg_trotter(couplings, T, depth, p):
+                simulate_heisenberg_model(wires, couplings, T, depth, p)
+                return qml.state()
+
+            _ = heisenberg_trotter(couplings, T, depth, p)
         end = timer()
         runtimes.append(end - start)
 
