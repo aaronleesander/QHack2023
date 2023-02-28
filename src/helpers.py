16b3d4786c92b0a376202c8b8d7272dff115d34b
EmilianoG-byte <57567043+EmilianoG-byte@users.noreply.github.com>
2023-02-28 12:44:52 +0100

    add further montecarlo analysis and experiment

--- src/helpers.py
+++ src/helpers.py
@@ -36,9 +36,31 @@ def calculate_mean(obs:np.ndarray, vectors:list[np.ndarray]):
         m += qml.math.fidelity(obs,v)
     return m/R
 
+def calculate_mean_paper(obs:np.ndarray, vectors:list[np.ndarray]):
+    s = np.zeros_like(vectors[0])
+    R = len(vectors)
+    for v in vectors:
+        s += abs(v)**2
+    phi = s/R
+    sigma_exact = np.diag(obs)
+    f = abs(np.vdot(np.sqrt(phi), np.sqrt(sigma_exact)))**2
+    return f
+
+def calculate_avg_dms(vectors:list[np.ndarray]):
+    r"calculate average density matrix for list of \ket{phi^r}"
+    
+    R = len(vectors)
+    d = len(vectors[0])
+    rho = np.zeros((d,d), dtype=complex)
+    print(rho.shape)
+    for v in vectors:
+        rho += np.outer(v, v.conj())
+    rho_avg = (rho/R)
+    return rho_avg
 
 
-def calculate_std(obs:np.ndarray, vectors:list[np.ndarray], means=None, bias:bool=False):
+
+def calculate_std(obs:np.ndarray, vectors:list[np.ndarray], means=None, bias:bool=False, mode=str):
     """
     Calculate the standard deviation for the observable F(vectors)=fidelity 
 
@@ -61,22 +83,45 @@ def calculate_std(obs:np.ndarray, vectors:list[np.ndarray], means=None, bias:boo
         factor = R
 
     if not means:
-        mean = calculate_mean(obs=obs, vectors=vectors)
+        if mode=='normal':
+            mean = calculate_mean(obs=obs, vectors=vectors)
+        if mode=='paper':
+            mean = calculate_mean_paper(obs=obs, vectors=vectors)
     s = 0
     for v in vectors:
-        s += (qml.math.fidelity(obs,v) - mean)**2
+        if mode=='normal':
+            s += (qml.math.fidelity(obs,v) - mean)**2
+        if mode=='paper':
+            phi = abs(v)**2
+            rho_exact = np.diag(obs)
+            f =  abs(np.vdot(np.sqrt(phi), np.sqrt(rho_exact)))**2
+            s += ( f - mean)**2
     return np.sqrt(s/factor)
 
 
-def calculate_avg_dms(obs:np.ndarray, vectors:list[np.ndarray]):
+def calculate_std_vs_ntraj(obs:np.ndarray, vectors:list[np.ndarray], mode='paper'):
     "states: "
 
     stds = []
     Rmax = len(vectors)
     ntrajs = list(np.arange(0,Rmax+1,10))
     for idx in ntrajs[1:]:
-        stds.append(calculate_std(obs=obs,vectors=vectors[:idx]))
+        stds.append(calculate_std(obs=obs,vectors=vectors[:idx], mode=mode))
 
     exp = 1/np.sqrt(ntrajs[1:])
     scale = exp[1]/stds[1]
     return ntrajs, stds, exp, scale
+
+def calculate_mean_vs_ntraj(obs:np.ndarray, vectors:list[np.ndarray], mode='paper'):
+    "states: "
+
+    means = []
+    Rmax = len(vectors)
+    ntrajs = list(np.arange(0,Rmax+1,10))
+    for idx in ntrajs[1:]:
+        if mode=='paper':
+            means.append(calculate_mean_paper(obs=obs,vectors=vectors[:idx]))
+        if mode =='normal':
+            means.append(calculate_mean(obs=obs,vectors=vectors[:idx]))
+
+    return ntrajs, means
